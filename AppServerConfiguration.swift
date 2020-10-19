a3e2666e25206331d411312098626072804d305f
Dmitriy Polyakov <dmitriyap11@gmail.com>
2020-10-19 12:15:30 +0300

    Fix access

--- AppServerConfiguration.swift
+++ AppServerConfiguration.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 public struct ServerConfiguration {
-    let restUrl: String
-    let socketUrl: String
-    let target: String
+    public let restUrl: String
+    public let socketUrl: String
+    public let target: String
+    
+    public init(restUrl: String, socketUrl: String, target: String) {
+        self.restUrl = restUrl
+        self.socketUrl = socketUrl
+        self.target = target
+    }
 }
