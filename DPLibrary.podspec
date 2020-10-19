ad125e1a904722a3cac77584b5b1aaeb770ce79c
Dmitriy Polyakov <dmitriyap11@gmail.com>
2020-10-19 12:20:26 +0300

    Fix files

--- DPLibrary.podspec
+++ DPLibrary.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'DPLibrary'
-  s.version          = '1.3.0'
+  s.version          = '1.3.1'
   s.summary          = 'DP library'
   s.description      = 'A set of useful utilities'
   s.homepage         = 'https://github.com/DPLibs/DPLibrary'
---
+++ DPLibrary/StyleWrapper/StyleWrapper+UITextField+Props.swift
@@ -0,0 +1,30 @@
+import Foundation
+import UIKit
+
+extension StyleWrapper where Element: UITextField {
+    
+    static func textAlignment(_ value: NSTextAlignment) -> StyleWrapper {
+        return .wrap { textField in
+            textField.textAlignment = value
+        }
+    }
+    
+    static func keyboardType(_ value: UIKeyboardType) -> StyleWrapper {
+        return .wrap { textField in
+            textField.keyboardType = value
+        }
+    }
+    
+    static func autocapitalizationType(_ value: UITextAutocapitalizationType) -> StyleWrapper {
+        return .wrap { textField in
+            textField.autocapitalizationType = value
+        }
+    }
+    
+    static func textColor(_ value: UIColor) -> StyleWrapper {
+        return .wrap { textField in
+            textField.textColor = value
+        }
+    }
+    
+}
