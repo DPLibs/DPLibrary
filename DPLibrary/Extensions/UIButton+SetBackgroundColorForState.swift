b6232c6ce24e86ae053e072495db1c3fb819456b
Dmitriy Polyakov <dmitriyap11@gmail.com>
2020-10-20 20:23:44 +0300

    UIButton+SetBackgroundColorForState

---
+++ DPLibrary/Extensions/UIButton+SetBackgroundColorForState.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+public extension UIButton {
+    
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        self.clipsToBounds = true
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.setBackgroundImage(colorImage, for: forState)
+    }
+    
+}
