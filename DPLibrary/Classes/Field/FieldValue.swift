91d53d22351d1d3f6ec29a597e2496f7208e9175
Dmitriy Polyakov <dmitriyap11@gmail.com>
2021-03-13 02:37:52 +0300

    Updates

--- DPLibrary/Classes/Field/FieldValue.swift
+++ DPLibrary/Classes/Field/FieldValue.swift
@@ -1,5 +1,86 @@
 import Foundation
 
+public protocol FieldsForm {
+    associatedtype SetupData
+    associatedtype RequestData: Encodable
+    
+    func setupData(_ data: SetupData)
+    func requestData() -> RequestData
+}
+
+public class TestSetupData {
+    public let test1: String
+    public let test2: Int
+    
+    public init(test1: String, test2: Int) {
+        self.test1 = test1
+        self.test2 = test2
+    }
+}
+
+public struct TestRequestData: Encodable {
+    let test1: String?
+    let test2: Int?
+}
+
+public class TestFieldsForm: FieldsForm {
+    public let test1: Field<String>
+    public let test2: Field<Int>
+    
+    public init(test1: Field<String>, test2: Field<Int>) {
+        self.test1 = test1
+        self.test2 = test2
+    }
+    
+    public func setupData(_ data: TestSetupData) {
+        self.test1.value = data.test1
+        self.test2.value = data.test2
+    }
+    
+    public func requestData() -> TestRequestData {
+        .init(test1: self.test1.value, test2: self.test2.value)
+    }
+    
+    public func begin() {
+        let mirror = Mirror(reflecting: self)
+        var errors: [FieldValidation] = []
+        mirror.children.forEach({ child in
+            if let field = child.value as? FieldValidatable {
+                field.validate(with: .any)
+                errors += field.errors
+            }
+        })
+        print("!!!", errors)
+    }
+    
+}
+
+public protocol FieldValidatable {
+    var errors: [FieldValidation] { get }
+    
+    func validate(with mode: FieldValidation.Mode)
+}
+
+open class Field<ValueType: Encodable>: Encodable, FieldValidatable {
+    public var value: ValueType?
+    public let validations: FieldValidations
+    
+    public var errors: [FieldValidation] = []
+    
+    enum CodingKeys: String, CodingKey {
+        case value = "value"
+    }
+    
+    public init(validations: FieldValidations, value: ValueType?) {
+        self.validations = validations
+        self.value = value
+    }
+    
+    public func validate(with mode: FieldValidation.Mode) {
+        self.errors = validations.gotErrors(for: self.value, with: mode)
+    }
+}
+
 open class FieldValue {
     open var value: Any?
     public let validations: FieldValidations
